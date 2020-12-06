b7c55d680d234933a846f5149121c39bbf4d2ad5
sunshuai <sunshuai.edu@gmail.com>
2020-12-06 17:10:02 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'blockExplorer',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    proxy: true,
+    prefix: '/api',
+    credentials: true
+  },
+
+  proxy: {
+    '/api': {
+      target: 'http://219.216.86.222:18334',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api' : '/'
+      }
+    }
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    transpile: [/^element-ui/],
+    vendor: ['axios']
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    创世区块hash：{{ genesisBlockHash }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      genesisBlockHash: ''
+    }
+  },
+  methods: {},
+  mounted() {
+    axios.post('api', {
+      "method": "getblockhash",
+      "params": [0]
+    }).then((response) => {
+      this.genesisBlockHash = response.data.result
+    })
+  }
+}
+</script>
+
+<style>
+
+
+</style>
