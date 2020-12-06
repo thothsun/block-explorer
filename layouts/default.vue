76a85d2f98d2f8d3044ca88780d055b28efbc8ce
sunshuai <sunshuai.edu@gmail.com>
2020-12-06 21:43:46 +0800

    finish requesting block basic information and rendering view

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,31 @@
 <template>
   <div>
-    <Nuxt />
+    <Nuxt/>
+    <a href="https://github.com/Sun-Shuai/blockExplorer" target="_blank">
+      <el-image style="position: fixed; top: 0; right: 0; border: 0;z-index: 9999;height: 100px;width: 100px"
+                src="/github.png"
+                alt="Fork me on GitHub"></el-image>
+    </a>
   </div>
 </template>
 
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  html {
+    font-size: 62.5%;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  body {
+    overflow-y: scroll;
+    min-height: 100vh;
+    font-size: 1.4rem;
+    background-color: white;
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,111 @@
 <template>
-  <div>
-    创世区块hash：{{ genesisBlockHash }}
+  <div style="width: 80%;margin: 0 auto">
+
+    <h1 style="margin-top: 20px">Block Explorer</h1>
+
+    <el-divider></el-divider>
+
+    <div style="display: flex;align-items: start">
+
+      <div style="width: 30%;display: inline-block">
+        <h3>Network State</h3>
+
+        <div style="width: 300px">
+          <div class="base-info">
+            <span>block counts</span>
+            <span>{{blockCount}}</span>
+          </div>
+
+          <div class="base-info">
+            <span>difficulty</span>
+            <span>{{currentDifficulty}}</span>
+          </div>
+
+        </div>
+      </div>
+
+      <div style="width: 70%;display: inline-block">
+        <h3>Recent Blocks</h3>
+
+        <el-table
+          :data="blockList"
+          style="width: 80%"
+          border>
+          <el-table-column
+            label="height"
+            prop="height"
+            min-width="20">
+          </el-table-column>
+
+          <el-table-column
+            label="hash"
+            prop="hash"
+            min-width="80">
+          </el-table-column>
+
+        </el-table>
+      </div>
+
+    </div>
+
+    <el-divider></el-divider>
+
+    <div>
+      <h3>Block Detail</h3>
+
+      <div style="width: 50%">
+        <div class="base-info">
+          <span>height</span>
+          <span>{{height}}</span>
+        </div>
+        <div class="base-info">
+          <span>confirmations</span>
+          <span>{{confirmations}}</span>
+        </div>
+        <div class="base-info">
+          <span>size</span>
+          <span>{{size}}</span>
+        </div>
+        <div class="base-info">
+          <span>time</span>
+          <span>{{time}}</span>
+        </div>
+        <div class="base-info">
+          <span>nonce</span>
+          <span>{{nonce}}</span>
+        </div>
+        <div class="base-info">
+          <span>bits</span>
+          <span>{{bits}}</span>
+        </div>
+        <div class="base-info">
+          <span>difficulty</span>
+          <span>{{difficulty}}</span>
+        </div>
+        <div class="base-info">
+          <span>merkleroot</span>
+          <span>{{merkleroot}}</span>
+        </div>
+        <div class="base-info">
+          <span>hash</span>
+          <span>{{hash}}</span>
+        </div>
+        <div class="base-info">
+          <span>previousblockhash</span>
+          <span>{{previousblockhash}}</span>
+        </div>
+        <div class="base-info">
+          <span>nextblockhash</span>
+          <span>{{nextblockhash}}</span>
+        </div>
+
+
+      </div>
+
+    </div>
+
+    <el-divider></el-divider>
+
   </div>
 </template>
 
@@ -11,27 +116,107 @@
   export default {
     data() {
       return {
-        genesisBlockHash: ''
+        //part1
+        blockCount: '-',
+        currentDifficulty: '-',
+        //part2
+        blockList: [],
+        //part3
+        height: '-',
+        confirmations: '-',
+        size: '-',
+        time: '-',
+        nonce: '-',
+        bits: '-',
+        difficulty: '-',
+        merkleroot: '-',
+        hash: '-',
+        previousblockhash: '-',
+        nextblockhash: '-',
       }
     },
-    methods: {},
-    mounted() {
-      axios.post('api', {
-        "method": "getblockhash",
-        "params": [0],
-      }, {
-        auth: {
-          username: username,
-          password: password
+    methods: {
+      requestRpc(method, params, callback) {
+        axios.post('api', {
+          method, params
+        }, {
+          auth: {
+            username: username,
+            password: password
+          }
+        }).then((response) => {
+          console.log(method + ': ' + response.data.result);
+          callback(response.data.result);
+        })
+      },
+
+      getBlockchainInfo() {
+        this.requestRpc('getblockchaininfo', null, (result) => {
+          this.blockCount = result.blocks;
+          this.currentDifficulty = result.difficulty;
+
+          this.getRecentBlocks();
+          this.getBlockDetail(this.blockCount);
+        });
+      },
+
+      getRecentBlocks() {
+        let times = this.blockCount;
+
+        for (let i = 0; i < 5; i++) {
+          if (times - i < 0) break;
+          this.requestRpc('getblockhash', [times - i], (result) => {
+            this.blockList.push({
+              'height': times - i,
+              'hash': result
+            });
+          })
         }
-      }).then((response) => {
-        this.genesisBlockHash = response.data.result
-      })
+
+      },
+
+
+      getBlockDetail(height) {
+        this.requestRpc('getblockhash', [height], (result) => {
+          this.requestRpc('getblock', ['0000a62f70b04c1543aa129a10065f435f258f4a25f1c6f996bcd7e33b6f59de'], (result) => {
+            this.height = result.height;
+            this.confirmations = result.confirmations;
+            this.size = result.size;
+            this.time = result.time;
+            this.nonce = result.nonce;
+            this.bits = result.bits;
+            this.difficulty = result.difficulty;
+            this.merkleroot = result.merkleroot;
+            this.hash = result.hash;
+            this.previousblockhash = result.previousblockhash;
+            this.nextblockhash = result.nextblockhash;
+          });
+        })
+      }
+    },
+    mounted() {
+      this.getBlockchainInfo();
+
+
     }
   }
 </script>
 
 <style>
+  h3 {
+    margin: 12px 0;
+  }
+
+  .base-info {
+    display: flex;
+    justify-content: space-between;
+    color: dimgray;
+    margin: 5px 0;
+  }
+
+  .base-info span:first-child {
+    font-weight: bolder;
+  }
 
 
 </style>
